9595891582a99429c0416594a6454715f044b802
rohit rathi <rohit.rathi@green-apex.com>
2016-12-27 21:01:01 +0530

    Connection closed but Some methods that return some values is not working.

--- src/main/java/com/ga/dao/impl/EventDaoImpl.java
+++ src/main/java/com/ga/dao/impl/EventDaoImpl.java
@@ -21,110 +21,150 @@ public class EventDaoImpl implements IEventDao {
     static {
         try {
             connection = DatabaseConnection.doConnection();
-        } catch (SQLException e) {
+        } catch (SQLException | InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
 
     public void insertEvent(Event event, int dbUserId) throws SQLException {
-
-        PreparedStatement pst = connection.prepareStatement("insert into event values (?,?,?,?,?,?,?,?,?,?,?,?)");
-        pst.setInt(1, 0);
-        pst.setString(2, event.getEventTitle());
-        pst.setString(3, event.getArtistName());
-        pst.setDate(4, new java.sql.Date(event.getStartDate().getTime()));
-        pst.setDate(5, new java.sql.Date(event.getEndDate().getTime()));
-        pst.setTime(6, new Time(event.getStartTime().getTime()));
-        pst.setTime(7, new Time(event.getEndTime().getTime()));
-        pst.setString(8, event.getLocation());
-        pst.setString(9, event.getDescription());
-        pst.setBlob(10, event.getEventImage());
-        pst.setString(11, event.getGenre());
-        pst.setInt(12, dbUserId);
-        pst.executeUpdate();
+        try {
+            PreparedStatement pst = connection.prepareStatement("insert into event values (?,?,?,?,?,?,?,?,?,?,?,?)");
+            pst.setInt(1, 0);
+            pst.setString(2, event.getEventTitle());
+            pst.setString(3, event.getArtistName());
+            pst.setDate(4, new java.sql.Date(event.getStartDate().getTime()));
+            pst.setDate(5, new java.sql.Date(event.getEndDate().getTime()));
+            pst.setTime(6, new Time(event.getStartTime().getTime()));
+            pst.setTime(7, new Time(event.getEndTime().getTime()));
+            pst.setString(8, event.getLocation());
+            pst.setString(9, event.getDescription());
+            pst.setBlob(10, event.getEventImage());
+            pst.setString(11, event.getGenre());
+            pst.setInt(12, dbUserId);
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
     public List<Event> viewMyEvents(int dbUserId) throws SQLException {
-
-        PreparedStatement pst = connection.prepareStatement("select * from event where user_id = ?");
-        pst.setInt(1, dbUserId);
         List<Event> myEventList = new ArrayList<Event>();
-        ResultSet rs = pst.executeQuery();
-        while (rs.next()) {
+        try {
+            PreparedStatement pst = connection.prepareStatement("select * from event where user_id = ?");
+            pst.setInt(1, dbUserId);
 
-            Event dbEvent = new Event();
-            dbEvent.setEventId(rs.getInt("event_id"));
-            dbEvent.setEventTitle(rs.getString("event_title"));
-            Blob imageBlob = (Blob) rs.getBlob("event_image");
-            // byte[] binaryStream = imageBlob.getBytes(1, (int) imageBlob.length());
-            InputStream binaryStream = imageBlob.getBinaryStream();
-            // InputStream binaryStream = rs.getBinaryStream("event_image");
-            dbEvent.setEventImage(binaryStream);
-            myEventList.add(dbEvent);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+
+                Event dbEvent = new Event();
+                dbEvent.setEventId(rs.getInt("event_id"));
+                dbEvent.setEventTitle(rs.getString("event_title"));
+                Blob imageBlob = (Blob) rs.getBlob("event_image");
+                InputStream binaryStream = imageBlob.getBinaryStream();
+                dbEvent.setEventImage(binaryStream);
+                myEventList.add(dbEvent);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
         return myEventList;
     }
 
     public List<Event> viewEventDetails(Integer eventId) throws SQLException {
 
-        PreparedStatement pst = connection.prepareStatement("select * from event where event_id = ?");
-        pst.setInt(1, eventId);
         List<Event> anEventDetail = new ArrayList<Event>();
-        ResultSet rs = pst.executeQuery();
-        if (rs.next()) {
 
-            Event dbEvent = new Event();
-            dbEvent.setEventId(rs.getInt("event_id"));
-            dbEvent.setEventTitle(rs.getString("event_title"));
-            dbEvent.setArtistName(rs.getString("artist_name"));
-            dbEvent.setStartDate(rs.getDate("event_start_date"));
-            dbEvent.setEndDate(rs.getDate("event_end_date"));
-            dbEvent.setStartTime(rs.getDate("event_start_time"));
-            dbEvent.setEndTime(rs.getDate("event_end_time"));
-            dbEvent.setLocation(rs.getString("location"));
-            dbEvent.setDescription(rs.getString("description"));
-            Blob imageBlob = (Blob) rs.getBlob("event_image");
-            InputStream binaryStream = imageBlob.getBinaryStream(1, imageBlob.length());
-            dbEvent.setEventImage(binaryStream);
-            dbEvent.setGenre(rs.getString("genre"));
-            anEventDetail.add(dbEvent);
+        try {
+            PreparedStatement pst = connection.prepareStatement("select * from event where event_id = ?");
+            pst.setInt(1, eventId);
+
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+
+                Event dbEvent = new Event();
+                dbEvent.setEventId(rs.getInt("event_id"));
+                dbEvent.setEventTitle(rs.getString("event_title"));
+                dbEvent.setArtistName(rs.getString("artist_name"));
+                dbEvent.setStartDate(rs.getDate("event_start_date"));
+                dbEvent.setEndDate(rs.getDate("event_end_date"));
+                dbEvent.setStartTime(rs.getTime("event_start_time"));
+                dbEvent.setEndTime(rs.getTime("event_end_time"));
+                dbEvent.setLocation(rs.getString("location"));
+                dbEvent.setDescription(rs.getString("description"));
+                dbEvent.setGenre(rs.getString("genre"));
+                anEventDetail.add(dbEvent);
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Error::- " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
         return anEventDetail;
     }
 
     public void editMyEventDetails(Event event, Integer eventId) throws SQLException {
-
-        PreparedStatement pst = connection
-                .prepareStatement("update event set event_title=?,artist_name=?,event_start_date=?,event_end_date=?,event_start_time=?,event_end_time=?,location=?,description=?,event_image=?,genre=? where event_id=?");
-        pst.setString(1, event.getEventTitle());
-        pst.setString(2, event.getArtistName());
-        pst.setDate(3, new java.sql.Date(event.getStartDate().getTime()));
-        pst.setDate(4, new java.sql.Date(event.getEndDate().getTime()));
-        pst.setTime(5, new Time(event.getStartTime().getTime()));
-        pst.setTime(6, new Time(event.getEndTime().getTime()));
-        pst.setString(7, event.getLocation());
-        pst.setString(8, event.getDescription());
-        pst.setBlob(9, event.getEventImage());
-        pst.setString(10, event.getGenre());
-        pst.setInt(11, eventId);
-        pst.executeUpdate();
+        try {
+            PreparedStatement pst = connection
+                    .prepareStatement("update event set event_title=?,artist_name=?,event_start_date=?,event_end_date=?,event_start_time=?,event_end_time=?,location=?,description=?,event_image=?,genre=? where event_id=?");
+            pst.setString(1, event.getEventTitle());
+            pst.setString(2, event.getArtistName());
+            pst.setDate(3, new java.sql.Date(event.getStartDate().getTime()));
+            pst.setDate(4, new java.sql.Date(event.getEndDate().getTime()));
+            pst.setTime(5, new Time(event.getStartTime().getTime()));
+            pst.setTime(6, new Time(event.getEndTime().getTime()));
+            pst.setString(7, event.getLocation());
+            pst.setString(8, event.getDescription());
+            pst.setBlob(9, event.getEventImage());
+            pst.setString(10, event.getGenre());
+            pst.setInt(11, eventId);
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
     public int getUserIdByEventId(Integer eventId) throws SQLException {
 
-        PreparedStatement pst = connection.prepareStatement("select user_id from event where event_id=?");
-        pst.setInt(1, eventId);
         int dbUserId = 0;
-        
-        ResultSet rs = pst.executeQuery();
-        if (rs.next()) {
-            dbUserId = rs.getInt("user_id");
+
+        try {
+            PreparedStatement pst = connection.prepareStatement("select user_id from event where event_id=?");
+            pst.setInt(1, eventId);
+
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                dbUserId = rs.getInt("user_id");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error::: " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
         return dbUserId;
     }
 
-    public Event getEventById(Integer eventId) {
+    public Event getEventById(Integer eventId) throws SQLException {
+
         Event dbEvent = new Event();
+
         try {
             PreparedStatement pst = connection.prepareStatement("select event_image from event where event_id=?");
             pst.setInt(1, eventId);
@@ -135,8 +175,12 @@ public class EventDaoImpl implements IEventDao {
                 InputStream binaryStream = imageBlob.getBinaryStream();
                 dbEvent.setEventImage(binaryStream);
             }
-        } catch (Exception e) {
-            System.err.println("ERROR:: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Error:: " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
         return dbEvent;
     }
--- src/main/java/com/ga/dao/impl/UserDaoImpl.java
+++ src/main/java/com/ga/dao/impl/UserDaoImpl.java
@@ -18,94 +18,148 @@ public class UserDaoImpl implements IUserDao {
     static {
         try {
             connection = DatabaseConnection.doConnection();
-        } catch (SQLException e) {
+        } catch (SQLException | InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
 
     public boolean registerUser(User user) throws SQLException {
 
-        PreparedStatement pst = connection.prepareStatement("select email from user where email=?");
-        pst.setString(1, user.getUserEmail());
+        try {
+            PreparedStatement pst = connection.prepareStatement("select email from user where email=?");
+            pst.setString(1, user.getUserEmail());
 
-        ResultSet rs = pst.executeQuery();
-        if (rs.next()) {
-            return false;
-        } else {
-            return addUser(user);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                return false;
+            } else {
+                return addUser(user);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
+            // return addUser(user);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
+        return false;
+
     }
 
     public boolean addUser(User user) throws SQLException {
+        try {
+            PreparedStatement pst = connection.prepareStatement("insert into user values (?,?,?,?,?,?,?)");
+            pst.setInt(1, 0);
+            pst.setString(2, user.getUserFirstName());
+            pst.setString(3, user.getUserLastName());
+            pst.setString(4, user.getUserGender());
+            pst.setLong(5, user.getUserContact());
+            pst.setString(6, user.getUserEmail());
+            pst.setString(7, user.getUserPassword());
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
 
-        PreparedStatement pst = connection.prepareStatement("insert into user values (?,?,?,?,?,?,?)");
-        pst.setInt(1, 0);
-        pst.setString(2, user.getUserFirstName());
-        pst.setString(3, user.getUserLastName());
-        pst.setString(4, user.getUserGender());
-        pst.setLong(5, user.getUserContact());
-        pst.setString(6, user.getUserEmail());
-        pst.setString(7, user.getUserPassword());
-        pst.executeUpdate();
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
         return true;
     }
 
     public boolean loginUser(String email, String password) throws SQLException {
 
-        PreparedStatement pst = connection
-                .prepareStatement("select email,password from user where email=? and password=?");
-        pst.setString(1, email);
-        pst.setString(2, password);
+        try {
+            PreparedStatement pst = connection
+                    .prepareStatement("select email,password from user where email=? and password=?");
+            pst.setString(1, email);
+            pst.setString(2, password);
 
-        ResultSet rs = pst.executeQuery();
-        if (rs.next()) {
-            return true;
-        } else {
-            return false;
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
+            // return false;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
+        return true;
     }
 
     public int getUserIdByEmail(String email) throws SQLException {
 
-        PreparedStatement pst = connection.prepareStatement("select user_id from user where email = ?");
-        pst.setString(1, email);
-
         int dbUserId = 0;
-        ResultSet rs = pst.executeQuery();
-        while (rs.next()) {
-            dbUserId = rs.getInt("user_id");
+        try {
+            PreparedStatement pst = connection.prepareStatement("select user_id from user where email = ?");
+            pst.setString(1, email);
+
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                dbUserId = rs.getInt("user_id");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
         return dbUserId;
     }
 
     public List<User> getUserDetails(int dbUserId) throws SQLException {
-        PreparedStatement pst = connection.prepareStatement("select * from user where user_id = ?");
-        pst.setInt(1, dbUserId);
+
         List<User> userDetails = new ArrayList<User>();
-        ResultSet rs = pst.executeQuery();
-        if (rs.next()) {
-            User dbUser = new User();
-            dbUser.setUserId(rs.getInt("user_id"));
-            dbUser.setUserFirstName(rs.getString("first_name"));
-            dbUser.setUserLastName(rs.getString("last_name"));
-            dbUser.setUserGender(rs.getString("gender"));
-            dbUser.setUserContact(rs.getLong("contact"));
-            dbUser.setUserEmail(rs.getString("email"));
-            dbUser.setUserPassword(rs.getString("password"));
-            userDetails.add(dbUser);
+        try {
+            PreparedStatement pst = connection.prepareStatement("select * from user where user_id = ?");
+            pst.setInt(1, dbUserId);
+
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                User dbUser = new User();
+                dbUser.setUserId(rs.getInt("user_id"));
+                dbUser.setUserFirstName(rs.getString("first_name"));
+                dbUser.setUserLastName(rs.getString("last_name"));
+                dbUser.setUserGender(rs.getString("gender"));
+                dbUser.setUserContact(rs.getLong("contact"));
+                dbUser.setUserEmail(rs.getString("email"));
+                dbUser.setUserPassword(rs.getString("password"));
+                userDetails.add(dbUser);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
         return userDetails;
     }
 
     public void editUserDetails(User user, Integer userId) throws SQLException {
 
-        PreparedStatement pst = connection.prepareStatement("update user set first_name=?,last_name=?, gender=?, contact=?,email=? where user_id=?");
-        pst.setString(1, user.getUserFirstName());
-        pst.setString(2, user.getUserLastName());
-        pst.setString(3, user.getUserGender());
-        pst.setLong(4, user.getUserContact());
-        pst.setString(5, user.getUserEmail());
-        pst.setInt(6, userId);
-        pst.executeUpdate();
+        try {
+            PreparedStatement pst = connection
+                    .prepareStatement("update user set first_name=?,last_name=?, gender=?, contact=?,email=? where user_id=?");
+            pst.setString(1, user.getUserFirstName());
+            pst.setString(2, user.getUserLastName());
+            pst.setString(3, user.getUserGender());
+            pst.setLong(4, user.getUserContact());
+            pst.setString(5, user.getUserEmail());
+            pst.setInt(6, userId);
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error::" + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 }
