e5df11cf2be106e58f2425db8d5f52a0bcc50c6a
rohit rathi <rohit.rathi@green-apex.com>
2016-12-27 21:01:57 +0530

    singleton pattern is implemented.

--- src/main/java/com/ga/util/DatabaseConnection.java
+++ src/main/java/com/ga/util/DatabaseConnection.java
@@ -6,16 +6,30 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-        public static Connection doConnection() throws SQLException {
-            Connection con = null;
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                con = DriverManager.getConnection("jdbc:mysql://localhost/");
-                return con;
-            } catch (ClassNotFoundException e) {
-                System.out.println("Connection Error is:- " + e.getException());
-                e.printStackTrace();
-            }
-            return null;
+    private static DatabaseConnection dbConnection;
+
+    private DatabaseConnection() {
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (dbConnection == null) {
+            dbConnection = new DatabaseConnection();
         }
+        return dbConnection;
+    }
+
+    public static Connection doConnection() throws SQLException, InstantiationException, IllegalAccessException {
+        
+        Connection con = null;
+     
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost/");
+            return con;
+        } catch (ClassNotFoundException e) {
+            System.out.println("Connection Error is:- " + e.getException());
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
