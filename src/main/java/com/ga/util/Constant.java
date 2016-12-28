4ec1f4933d54c77675e9e2febc7335d08b1334f3
rohit rathi <rohit.rathi@green-apex.com>
2016-12-28 20:57:18 +0530

    new constants added

--- src/main/java/com/ga/util/Constant.java
+++ src/main/java/com/ga/util/Constant.java
@@ -3,10 +3,24 @@ package com.ga.util;
 public class Constant {
 
     public static final String REGESTRATION_ERROR_MESSAGE = "You Are Already Registered With This Mail";
-    
-    public static final String REGESTRATION_SUCCESSFUL_MESSAGE = "Registered Successfully"; 
-
+    public static final String REGESTRATION_SUCCESSFUL_MESSAGE = "Registered Successfully";
     public static final String LOGIN_ERROR_MESSAGE = "Email or Password Don't Match";
-    
     public static final String LOGIN_SUCCESSFUL_MESSAGE = "Welcome..!! Your Events Are Listed Below";
+
+    public static final String REGISTER_USER = "register";
+    public static final String LOGIN_USER = "login";
+    public static final String EDIT_USER = "editUser";
+    public static final String LOGOUT = "logout";
+
+    public static final String ADD_EVENT = "addEvent";
+    public static final String VIEW_EVENT = "viewEvent";
+    public static final String UPDATE = "update";
+    public static final String EDIT_EVENT = "editEvent";
+    public static final String UPDATE_USER = "updateUser";
+
+    public static final String BLANK_CONTACT = "Contact Should Not Be Blank";
+    public static final String LESS_CONTACT_DIGIT = "Minimum 10 Digits Required In Contact Field";
+    public static final String BLANK_EMAIL = "Email Is Required For Registration";
+    public static final String BLANK_PASSWORD_WITH_SPACE = "Password Shouldn't Be Blank Or Contain Space";
+    public static final String LESS_PASSWORD_CHAR = "Password Should Be Greater Than 6 Digit";
 }
